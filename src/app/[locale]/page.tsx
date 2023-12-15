1cb553d1547f59d4ccf86f15c3b3fdb4890c8063
burakkurt <krtburak@outlook.com>
2023-12-15 23:19:21 +0300

    multilang skeleton added

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,13 @@
 import { useTranslations } from 'next-intl';
+import HomeContainer from '@/containers/home/page';
 
 export default function Index() {
   const t = useTranslations('Index');
 
-  return <h1>{t('title')}</h1>;
+  return (
+    <>
+      <h1>{t('title')}</h1>
+      <HomeContainer />
+    </>
+  );
 }
