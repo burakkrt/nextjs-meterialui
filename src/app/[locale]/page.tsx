f2eab8f11dd4bcd8dea9ac1eb44c45b04f76ee62
burakkurt <krtburak@outlook.com>
2023-12-16 02:39:33 +0300

    static page rndering

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,22 @@
-import { useTranslations } from 'next-intl';
 import HomeContainer from '@/containers/home/page';
+import { unstable_setRequestLocale } from 'next-intl/server';
+import { getTranslations } from 'next-intl/server';
+import { useTranslations } from 'next-intl';
+import { IParams } from './types';
 
-export default function Index() {
+// Static meta tags rendering
+export async function generateMetadata({ params: { locale } }: IParams) {
+  const t = await getTranslations({ locale, namespace: 'Metadata' });
+
+  return {
+    title: t('title'),
+  };
+}
+
+export default function Index({ params: { locale } }: IParams) {
+  unstable_setRequestLocale(locale);
   const t = useTranslations('Index');
+
   return (
     <>
       <h1>{t('title')}</h1>
