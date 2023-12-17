10713da40507f3d7d343a0b6698a0184e1b54059
burakkurt <krtburak@outlook.com>
2023-12-17 15:32:31 +0300

    meta tags modified

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,21 +12,38 @@ import {
 export async function generateMetadata({ params: { locale } }: IParams) {
   const multiMetadata = await getTranslations({ locale, namespace: 'Metadata' });
 
+  // icons ve apple-icons eklemek için takip et :  https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons#image-files-ico-jpg-png
   return {
     title: multiMetadata('title'),
     description: multiMetadata('description'),
     keywords: multiMetadata('keywords'),
     metadataBase: new URL(multiMetadata('metadataBase.baseUrl')),
+    applicationName: multiMetadata('applicationName'),
     alternates: {
       canonical: '/',
       languages: langCanonicalUrls(locales, locale),
     },
     openGraph: {
-      //og:title, og:description yerine geçer. genel sosyal medyalarda görünüm.
       title: multiMetadata('openGraph.title'),
       description: multiMetadata('openGraph.description'),
       locale: multiMetadata('openGraph.locale'),
       alternateLocale: langMetaConvert(locales, locale),
+      url: multiMetadata('openGraph.url'),
+      type: 'website',
+      siteName: multiMetadata('openGraph.siteName'),
+      images: [
+        {
+          url: '...',
+          width: 800,
+          height: 600,
+        },
+        {
+          url: '...',
+          width: 1800,
+          height: 1600,
+          alt: multiMetadata('openGraph.imagesAlt'),
+        },
+      ],
     },
   };
 }
