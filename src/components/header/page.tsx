4898c1d4fd4f55d96764e114cbaa8df117ff009e
burakkurt <krtburak@outlook.com>
2023-12-17 10:11:16 +0300

    add mode ui

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -24,6 +24,11 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import * as Styled from './page.styled';
 import { IRootParams } from './types';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { useTheme } from '@mui/material/styles';
+import Switch from '@mui/material/Switch';
+import { styled } from '@mui/material/styles';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -31,6 +36,7 @@ const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 const HeaderPage = ({ locale, langHeader: pages }: IRootParams) => {
   const [mobileRightMenu, setMobileRightMenu] = React.useState<boolean>(false);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const theme = useTheme();
 
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
@@ -51,6 +57,56 @@ const HeaderPage = ({ locale, langHeader: pages }: IRootParams) => {
 
     setMobileRightMenu(state);
   };
+
+  const MaterialUISwitch = styled(Switch)(({ theme }) => {
+    console.log(theme.palette.mode);
+    return {
+      width: 62,
+      height: 34,
+      padding: 7,
+      '& .MuiSwitch-switchBase': {
+        margin: 1,
+        padding: 0,
+        transform: 'translateX(6px)',
+        '&.Mui-checked': {
+          color: '#fff',
+          transform: 'translateX(22px)',
+          '& .MuiSwitch-thumb:before': {
+            backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+              '#fff'
+            )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+          },
+          '& + .MuiSwitch-track': {
+            opacity: 1,
+            backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+          },
+        },
+      },
+      '& .MuiSwitch-thumb': {
+        backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+        width: 32,
+        height: 32,
+        '&:before': {
+          content: "''",
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          left: 0,
+          top: 0,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+            '#fff'
+          )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+        },
+      },
+      '& .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+        borderRadius: 20 / 2,
+      },
+    };
+  });
 
   return (
     <AppBar position="static">
@@ -165,6 +221,19 @@ const HeaderPage = ({ locale, langHeader: pages }: IRootParams) => {
               // </Typography>
             ))}
           </Box>
+
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <MaterialUISwitch sx={{ m: 1 }} defaultChecked={theme.palette.mode === 'dark'} />
+              }
+              label={
+                theme.palette.mode.slice(0, 1).toLocaleUpperCase() +
+                theme.palette.mode.slice(1) +
+                ' Mode'
+              }
+            />
+          </FormGroup>
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
--- src/components/theme-provider/ThemeProvider.tsx
+++ src/components/theme-provider/ThemeProvider.tsx
@@ -1,15 +1,49 @@
 'use client';
+import { useState, useMemo } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { IRootProps } from './types';
+import { PaletteMode } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const handleCreateTheme = (mode: PaletteMode) => ({
+  typography: {
+    fontFamily: ['Alegreya', 'Roboto', '-apple-system'].join(','),
+  },
+  palette: {
+    mode,
+    body: {
+      ...(mode === 'light'
+        ? {
+            // palette values for light mode
+            test: 'red',
+          }
+        : {
+            // palette values for dark mode
+            test: 'yellow',
+          }),
+    },
+  },
+});
 
 function ThemeRootProvider({ children }: IRootProps) {
-  const theme = createTheme({
-    typography: {
-      fontFamily: ['Alegreya', 'Roboto', '-apple-system'].join(','),
-    },
-  });
+  const [mode, setMode] = useState<PaletteMode>('dark');
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode: PaletteMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    []
+  );
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  const theme = useMemo(() => createTheme(handleCreateTheme(mode)), [mode]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
 }
 
 export default ThemeRootProvider;
