2d9544b9d355fb66a008ae98cc948327d7167d46
burakkurt <krtburak@outlook.com>
2023-12-24 23:07:08 +0300

    i18n configre

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,13 @@
 import { getRequestConfig } from 'next-intl/server';
+import { notFound } from 'next/navigation';
 
 export const locales = ['tr', 'en'];
 
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`./languages/${locale}.json`)).default,
-}));
+export default getRequestConfig(async ({ locale }) => {
+  // Validate that the incoming `locale` parameter is valid
+  if (!locales.includes(locale as any)) notFound();
+
+  return {
+    messages: (await import(`./languages/${locale}.json`)).default,
+  };
+});
