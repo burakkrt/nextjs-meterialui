224fd370830b984d5a1211bbc4ce7b146b6801ea
burakkurt <krtburak@outlook.com>
2023-12-30 00:04:25 +0300

    deploying config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const withNextIntl = require('next-intl/plugin')();
 
 const nextConfig = withNextIntl({
   // Other Next.js configuration ...
+  images: {
+    unoptimized: true,
+  },
+  output: 'export',
+  trailingSlash: true,
 });
 
 module.exports = nextConfig;
